e581cde4d93197d612127c76928a9f050ad1d584
Sebatianbotia <122814675+Sebatianbotia@users.noreply.github.com>
2023-09-26 11:27:52 -0500

    Task 08 finished

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
+const galleryContainer = document.querySelector(".gallery")
+function createGallery (items){
+    return items
+    .map((item)=>`<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+       <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>
+ </li>`)
+ .join("");
+}
+
+const addGallery = createGallery(galleryItems);
+
+galleryContainer.innerHTML = addGallery
+
+function action (imgAction){
+    imgAction.preventDefault()
+}
+
+var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt' , captionDelay: 100 });
 console.log(galleryItems);
